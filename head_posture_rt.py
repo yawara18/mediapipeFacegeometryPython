53f40f49569b695f06d483ebe4cf0329eb0798a6
yawara18 <kimkiwan@team-lab.com>
2021-09-13 12:09:34 +0900

    change cam resolution & print angels

--- head_posture_rt.py
+++ head_posture_rt.py
@@ -8,13 +8,15 @@ from face_geometry import get_metric_landmarks, PCF, canonical_metric_landmarks,
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
-points_idx = [33,263,61,291,199]
-points_idx = points_idx + [key for (key,val) in procrustes_landmark_basis]
+points_idx = [33,263,61,291,199] #[33,263,61,291,199]
+points_idx = points_idx + [key for (key,val) in procrustes_landmark_basis] # 5 + 33
 points_idx = list(set(points_idx))
 points_idx.sort()
 # points_idx = list(range(0,468)); points_idx[0:2] = points_idx[0:2:-1];
 
-frame_height, frame_width, channels = (720, 1280, 3)
+#frame_height, frame_width, channels = (720, 1280, 3)
+frame_height, frame_width, channels = (480, 640, 3)
+
 
 # pseudo camera internals
 focal_length = frame_width
@@ -27,6 +29,23 @@ camera_matrix = np.array(
 
 dist_coeff = np.zeros((4, 1))
 
+def print_angles(rotation_vector, translation_vector):
+    #yaw pitch roll #https://answers.opencv.org/question/160041/how-to-get-angles-of-x-y-z-in-pose-estimation/
+    dst, jacobian = cv2.Rodrigues(rotation_vector) #convert 3x1 to 3x3
+
+    #https://www.itd-blog.jp/entry/peep-prevention-1
+    pose_mat = cv2.hconcat((dst, translation_vector))
+    _, _, _, _, _, _, euler_angle = cv2.decomposeProjectionMatrix(pose_mat)
+
+    #https://qiita.com/oozzZZZZ/items/1e68a7572bc5736d474e
+    yaw = euler_angle[1]
+    pitch = euler_angle[0]
+    roll = euler_angle[2]
+
+    # -180 ~ 180で返されるので、正面が０，上が+、下が-になるように
+    pitch = 180 - pitch if pitch > 0 else -(pitch + 180)
+    print('yaw : ', yaw, 'pitch : ', pitch, 'roll : ', roll)
+    
 def main():
     source = WebcamSource()
 
@@ -51,6 +70,9 @@ def main():
 
             success, rotation_vector, translation_vector = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeff, flags=cv2.cv2.SOLVEPNP_ITERATIVE)
             # _, rotation_vector, translation_vector, inliers = cv2.solvePnPRansac(model_points, image_points, camera_matrix, dist_coeff)
+            
+            #print('model_points', np.shape(model_points), model_points[0])
+            #print('image_points', np.shape(image_points), image_points[0])
 
             (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 25.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeff)
 
@@ -63,7 +85,7 @@ def main():
 
             frame = cv2.line(frame, p1, p2, (255,0,0), 2)
 
-
+            print_angles(rotation_vector, translation_vector)
 
         source.show(frame)
 
